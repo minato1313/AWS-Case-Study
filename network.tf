4291563be84c45aa1480bcfe8d6c64f6522c6c6d
Vishal Morchhle <143252661+minato1313@users.noreply.github.com>
2024-02-14 15:01:28 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -64,7 +64,6 @@ resource "aws_route_table" "public_subnet_route_table" {
 # Associate the public route table with the public subnets
 resource "aws_route_table_association" "public_subnet_association" {
   subnet_id      = aws_subnet.public_subnet_1.id
-  subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_subnet_route_table.id
 }
 
